4710aedf2e123aeb3027aaa2dc2c7e6a17e79380
szawarma <dawidoszczak035@gmail.com>
2025-11-03 15:23:19 +0000

    nowy

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,51 @@
 //dawid szarwas
 
 #include <iostream>
-int main() {
-    std::cout << "Hello, Matura 2025!\n";
+#include <fstream>
+
+using namespace std;
+
+string z3_1_path_symboe_txt = "../zalaczniki-2025/symbole.txt";
+
+bool czy_palindrom(string slowo)
+{
+    int dlugosc = slowo.length();
+    for(int i = 0; i < dlugosc / 2; i++)
+    {
+        if(slowo[i] != slowo[dlugosc - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void zadanie2_1()
+{
+    ifstream plik(z3_1_path_symboe_txt);
+    if(!plik.is_open())
+    {
+        cerr << "nie mozna otworzyc pliku" << endl;
+    }
+
+    string linia;
+    while(plik >> linia)
+    {
+        if (czy_palindrom(linia))
+        {
+            cout << linia << endl;
+        }
+    }
+
+
+    plik.close();
+
+}
+
+
+int main() 
+{
+
+    zadanie2_1();
     return 0;
 }
